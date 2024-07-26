d108d2a0e47c6ca2bc77cabafe4b1f8796222a88
Adi <adityamauryamaurya144@gmail.com>
2024-07-26 23:08:54 +0530

    First_Commit

---
+++ src/components/singlePost/SinglePost.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./singlePost.css";
+
+function SinglePost() {
+  return (
+    <div className='singlePost'>
+        SinglePost
+    </div>
+  )
+}
+
+export default SinglePost
